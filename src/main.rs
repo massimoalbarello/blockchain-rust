293a61fa1da349a446f55650759a89e6d4222225
massimoalbarello <albo.max@hotmail.it>
2022-07-23 11:32:33 +0200

    instantiate block and generate hash

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use blockchainlib::*;
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block!".to_owned());
+
+    let h = block.hash();
+    block.hash = h;
+    
+    println!("{:?}", &block);
+
+}
